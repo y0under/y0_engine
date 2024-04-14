c4697cbaad51e498322964054ee1cdd69eb200bc
y0ub <y0lit7515@gmail.com>
2024-04-14 21:52:14 +0900

    delete: delete unnesessary file.

--- y0_engine/y0_core/include/y0_core.h
+++
@@ -1,6 +0,0 @@
-#ifndef Y0_ENGINE_Y0_CORE_INCLUDE_Y0_CORE_H
-#define Y0_ENGINE_y0_CORE_INCLUDE_Y0_CORE_H
-
-#include "y0_core/vector2.h"
-
-#endif
--- y0_engine/y0_core/include/y0_core/vector2.h
+++
@@ -1,9 +0,0 @@
-#ifndef Y0_ENGINE_Y0_CORE_INCLUDE_Y0_CORE_VECTOR2_H
-#define Y0_ENGINE_Y0_CORE_INCLUDE_Y0_CORE_VECTOR2_H
-
-struct vector2 {
-  float x;
-  float y;
-};
-
-#endif
