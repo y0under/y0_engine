c4ff90ca38bac3b104f4cb3153e4c3826ee9eb66
y0ub <y0lit7515@gmail.com>
2024-04-14 21:38:50 +0900

    add: vector2.

---
+++ y0_engine/y0_core/include/y0_core.h
@@ -0,0 +1,6 @@
+#ifndef Y0_ENGINE_Y0_CORE_INCLUDE_Y0_CORE_H
+#define Y0_ENGINE_y0_CORE_INCLUDE_Y0_CORE_H
+
+#include "y0_core/vector2.h"
+
+#endif
---
+++ y0_engine/y0_core/include/y0_core/vector2.h
@@ -0,0 +1,9 @@
+#ifndef Y0_ENGINE_Y0_CORE_INCLUDE_Y0_CORE_VECTOR2_H
+#define Y0_ENGINE_Y0_CORE_INCLUDE_Y0_CORE_VECTOR2_H
+
+struct vector2 {
+  float x;
+  float y;
+};
+
+#endif
