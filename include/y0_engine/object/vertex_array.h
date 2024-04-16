5baf27fdfad7b33446dd23b1ec8d85ca8d02c168
y0ub <y0lit7515@gmail.com>
2024-04-16 22:08:54 +0900

    add: vertex_array class.

---
+++ include/y0_engine/object/vertex_array.h
@@ -0,0 +1,26 @@
+#ifndef Y0_ENGINE_INCLUDE_Y0_ENGINE_OBJECT_VERTEX_ARRAY_H
+#define Y0_ENGINE_INCLUDE_Y0_ENGINE_OBJECT_VERTEX_ARRAY_H
+
+#include <GLFW/glfw3.h>
+
+class vertex_array {
+  public:
+    // constructor for make vao
+    vertex_array(GLint dimension_, const float* vertex, GLuint vertex_count,
+                 const GLuint *index, GLuint index_count);
+    ~vertex_array();
+
+    void bind();
+
+    GLuint get_index_count() const { return index_count_; }
+    GLuint get_vertex_count() const { return vertex_count_; }
+  private:
+    GLint dimension_;
+    GLuint vertex_count_;
+    GLuint index_count_;
+    GLuint vbo_;
+    GLuint index_buffer_;
+    GLuint vao_;
+};
+
+#endif
