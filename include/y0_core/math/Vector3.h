4f15ce1b8a915f4d0f2faf4b3413bf7f641d6570
y0ub <y0lit7515@gmail.com>
2024-04-23 20:41:25 +0900

    add: Vector3.h

---
+++ include/y0_core/math/Vector3.h
@@ -0,0 +1,12 @@
+#ifndef Y0_ENGINE_INCLUDE_Y0_CORE_MATH_VECTOR3_H
+#define Y0_ENGINE_INCLUDE_Y0_CORE_MATH_VECTOR3_H
+
+template<typename T>
+struct Vector3 {
+  public:
+    T x;
+    T y;
+    T z;
+};
+
+#endif
