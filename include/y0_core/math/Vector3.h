cedb755ccab1e47ddd98a6453c6ea62ffca687f3
y0ub <y0lit7515@gmail.com>
2024-05-05 11:50:53 +0900

    update: defeine zero vector

--- include/y0_core/math/Vector3.h
+++ include/y0_core/math/Vector3.h
@@ -5,9 +5,20 @@ namespace y0_engine {
   template<typename T>
   struct Vector3 {
     public:
-      T x;
-      T y;
-      T z;
+      Vector3 Vector3() {
+        Vector3(static_cast<T>(0), static_cast<T>(0), static_cast<T>(0));
+      }
+
+      Vector3 Vector3(T x, T y, T z)
+        : x_(x), y_(y), z_(z)
+      {
+      }
+
+      T x_;
+      T y_;
+      T z_;
+
+      Vector3 zero(0.0f, 0.0f, 0.0f);
   };
 } // namespace y0_engine
 
