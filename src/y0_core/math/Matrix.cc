33f09ad97af0da8d4afd46b8b0534faee9b99ede
y0ub <y0lit7515@gmail.com>
2024-05-13 20:32:10 +0900

    update: namespace of class Matrix.

--- src/y0_core/math/Matrix.cc
+++ src/y0_core/math/Matrix.cc
@@ -2,14 +2,16 @@
 
 #include <algorithm>
 
-Matrix::Matrix() {
-}
+namespace y0_engine {
+  Matrix::Matrix() {
+  }
 
-Matrix::Matrix(const GLfloat *a) {
-  std::copy(a, a + 16, matrix);
-}
+  Matrix::Matrix(const GLfloat *a) {
+    std::copy(a, a + 16, matrix);
+  }
 
-void Matrix::load_identity() {
-  std::fill(matrix, matrix + 16, 0.0f);
-  matrix[0] = matrix[5] = matrix[10] = matrix[15] = 1.0f;
+  void Matrix::load_identity() {
+    std::fill(matrix, matrix + 16, 0.0f);
+    matrix[0] = matrix[5] = matrix[10] = matrix[15] = 1.0f;
+  }
 }
