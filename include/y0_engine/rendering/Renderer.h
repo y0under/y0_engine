2ffa2e4380fa1228c71fef8d25a9c824ceb47143
y0ub <y0lit7515@gmail.com>
2024-05-28 21:58:42 +0900

    update: renderer

--- include/y0_engine/rendering/Renderer.h
+++ include/y0_engine/rendering/Renderer.h
@@ -1,7 +1,23 @@
 #ifndef Y0_ENGINE_INCLUDE_Y0_ENGINE_RENDERING_RENDERER_H
 #define Y0_ENGINE_INCLUDE_Y0_ENGINE_RENDERING_RENDERER_H
 
-class Renderer {
-};
+#include <memory>
+#include <string>
+
+#include "y0_engine/window/WindowGlfw.h"
+
+namespace y0_engine {
+  class Renderer {
+    public:
+      Renderer(std::string &title = "default title");
+      ~Renderer();
+      void Draw();
+
+    private:
+      const float kwidth_ = 1500;
+      const float kheight_ = 843;
+      std::unique_ptr<WindowGlfw> window_;
+  };
+} // namespace y0_engine
 
 #endif
--- src/y0_engine/rendering/Renderer.cc
+++ src/y0_engine/rendering/Renderer.cc
@@ -1,1 +1,15 @@
 #include "y0_engine/rendering/Renderer.h"
+
+namespace y0_engine {
+  Renderer::Renderer(std::string &title) {
+    : window_(new WindowGlfw(kwidth_, kheight_, title) {
+  }
+
+  Renderer::~Renderer() {
+  }
+
+  void Renderer::Draw() {
+    // TODO: render mesh component
+    // TODO: render sprite component
+  }
+} // namespace y0_engine
