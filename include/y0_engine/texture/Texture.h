66e87578a03c96c0c81cd68d1d1ea6e7c8eb6431
y0ub <y0lit7515@gmail.com>
2024-06-01 17:12:26 +0900

    update: use namespace

--- include/y0_engine/texture/Texture.h
+++ include/y0_engine/texture/Texture.h
@@ -3,23 +3,25 @@
 
 #include <string>
 
-class Texture {
-public:
-	Texture();
-	~Texture();
-	
-	bool LoadTexture(const std::string &file_name);
-	void UnloadTexture();
-	
-	void LetActiveTheTexture();
+namespace y0_engine {
+  class Texture {
+    public:
+      Texture();
+      ~Texture();
 
-	inline int GetWidth() const { return width_; }
-	inline int GetHeight() const { return height_; }
+      bool LoadTexture(const std::string &file_name);
+      void UnloadTexture();
 
-private:
-	unsigned int texture_id_;
-	int width_;
-	int height_;
-};
+      void LetActiveTheTexture();
+
+      inline int GetWidth() const { return width_; }
+      inline int GetHeight() const { return height_; }
+
+    private:
+      unsigned int texture_id_;
+      int width_;
+      int height_;
+  };
+}  // namespace y0_engine
 
 #endif
--- src/y0_engine/texture/Texture.cc
+++ src/y0_engine/texture/Texture.cc
@@ -4,50 +4,52 @@
 
 #include "y0_engine/texture/Texture.h"
 
-Texture::Texture()
-  : texture_id_(0),
+namespace y0_engine {
+  Texture::Texture()
+    : texture_id_(0),
     width_(0),
     height_(0) {
-}
+    }
 
-Texture::~Texture() {
-  UnloadTexture();
-}
-
-bool Texture::LoadTexture(const std::string &file_name) {
-  // load image
-  cv::Mat image = cv::imread(file_name, cv::IMREAD_UNCHANGED);
-  if (image.empty()) {
-    std::cerr << "Failed to load texture: " << file_name << std::endl;
-    return false;
+  Texture::~Texture() {
+    UnloadTexture();
   }
 
-  // set format following the number of chanel.
-  int channels = image.channels();
-  int format = channels == 4 ? GL_RGBA : GL_RGB;
+  bool Texture::LoadTexture(const std::string &file_name) {
+    // load image
+    cv::Mat image = cv::imread(file_name, cv::IMREAD_UNCHANGED);
+    if (image.empty()) {
+      std::cerr << "Failed to load texture: " << file_name << std::endl;
+      return false;
+    }
 
-  width_ = image.cols;
-  height_ = image.rows;
+    // set format following the number of chanel.
+    int channels = image.channels();
+    int format = channels == 4 ? GL_RGBA : GL_RGB;
 
-  glGenTextures(1, &texture_id_);
-  glBindTexture(GL_TEXTURE_2D, texture_id_);
+    width_ = image.cols;
+    height_ = image.rows;
 
-  glTexImage2D(GL_TEXTURE_2D, 0, format, width_, height_, 0, format, GL_UNSIGNED_BYTE, image.data);
+    glGenTextures(1, &texture_id_);
+    glBindTexture(GL_TEXTURE_2D, texture_id_);
 
-  // let active linear filtering
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexImage2D(GL_TEXTURE_2D, 0, format, width_, height_, 0, format, GL_UNSIGNED_BYTE, image.data);
 
-  return true;
-}
+    // let active linear filtering
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-void Texture::LetActiveTheTexture() {
-  glBindTexture(GL_TEXTURE_2D, texture_id_);
-}
+    return true;
+  }
 
-void Texture::UnloadTexture() {
-  if (texture_id_ == 0)
-    return;
-  glDeleteTextures(1, &texture_id_);
-  texture_id_ = 0;
+  void Texture::LetActiveTheTexture() {
+    glBindTexture(GL_TEXTURE_2D, texture_id_);
+  }
+
+  void Texture::UnloadTexture() {
+    if (texture_id_ == 0)
+      return;
+    glDeleteTextures(1, &texture_id_);
+    texture_id_ = 0;
+  }
 }
