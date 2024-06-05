048d93fe833973e1d4b26dbb6688ba50ea96ca07
y0ub <y0lit7515@gmail.com>
2024-06-05 21:12:45 +0900

    update: add dox comment to Matrix.cc

--- src/y0_core/math/Matrix.cc
+++ src/y0_core/math/Matrix.cc
@@ -3,13 +3,24 @@
 #include <algorithm>
 
 namespace y0_engine {
+  /**
+   * @brief constructor
+   */
   Matrix::Matrix() {
   }
 
+  /**
+   * @brief constructor to set default value
+   *
+   * @param a initilalize value
+   */
   Matrix::Matrix(const GLfloat *a) {
     std::copy(a, a + 16, matrix);
   }
 
+  /**
+   * @brief let the matrix to identity matrix
+   */
   void Matrix::load_identity() {
     std::fill(matrix, matrix + 16, 0.0f);
     matrix[0] = matrix[5] = matrix[10] = matrix[15] = 1.0f;
