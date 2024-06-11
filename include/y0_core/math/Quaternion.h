94cbb6e337e5ddac8ca16cde1f2ee2bc3115d9d6
y0ub <y0lit7515@gmail.com>
2024-06-11 22:37:08 +0900

    update: quaternion.

--- include/y0_core/math/Quaternion.h
+++ include/y0_core/math/Quaternion.h
@@ -1,4 +1,17 @@
 namespace y0_engine {
   class Quaternion {
+    public:
+      Quaternion();
+      Quaternion(const float &x, const float &y, const float &z, const float &w);
+      void Set(const float &x, const float &y, const float &z, const float &w);
+
+      static const Quaternion identity_;
+
+    private:
+      float x_;
+      float y_;
+      float z_;
+      float w_;
+
   };
 } // namespace y0_engine
--- src/y0_core/math/Quaternion.cc
+++ src/y0_core/math/Quaternion.cc
@@ -2,7 +2,40 @@
 #include "y0_core/math/Quaternion.h"
 
 namespace y0_engine {
+  const Quaternion Quaternion::identity_(0.0f, 0.0f, 0.0f, 1.0f);
 
+  /**
+   * @brief constructor
+   */
+  Quaternion::Quaternion() {
+    *this = Quaternion::identity_;
+  }
+
+  /**
+   * @brief constructor with default value
+   *
+   * @param x
+   * @param y
+   * @param z
+   * @param w
+   */
+  Quaternion::Quaternion(const float &x, const float &y,const float &z,const float &w) {
+    Set(x, y, z, w);
+  }
+  /**
+   * @brief Set this value
+   *
+   * @param x
+   * @param y
+   * @param z
+   * @param w
+   */
+  void Quaternion::Set(const float &x, const float &y, const float &z, const float &w) {
+    x_ = x;
+    y_ = y;
+    z_ = z;
+    w_ = w;
+  }
 } // namespace y0_engine
 
 #endif
