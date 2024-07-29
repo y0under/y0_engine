a31d4fadb0f20e7ad23232ac7179943a865e9034
y0ub <y0lit7515@gmail.com>
2024-07-29 18:27:58 +0900

    update: add dox comment to vector3.

--- include/y0_core/math/Vector3.h
+++ include/y0_core/math/Vector3.h
@@ -5,27 +5,48 @@ namespace y0_engine {
   template<typename T>
   struct Vector3 {
     public:
+      /**
+       * @brief constructor
+       */
       explicit Vector3() {
         Vector3(static_cast<T>(0), static_cast<T>(0), static_cast<T>(0));
       }
 
+      /**
+       * @brief constructor with value
+       *
+       * @param x_value
+       * @param y_value
+       * @param z_value
+       */
       explicit Vector3(T x_value, T y_value, T z_value)
         : x(x_value), y(y_value), z(z_value)
       {
       }
 
-      T x;
-      T y;
-      T z;
-
+      /**
+       * @brief make zero vector and return this one.
+       *
+       * @return 
+       */
       static Vector3<float> get_zero_vector() {
         Vector3<float> v(0.0f, 0.0f, 0.0f);
         return v;
       }
 
+      /**
+       * @brief 
+       *
+       * @return 
+       */
       T SquareLength() {
         return x * x + y * y + z * z;
       }
+
+      // variable
+      T x;
+      T y;
+      T z;
   };
 } // namespace y0_engine
 
