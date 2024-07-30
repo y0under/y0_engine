752b0f93416e0bb9a32eff0508f0120fa7a8b86c
y0ub <y0lit7515@gmail.com>
2024-07-30 22:16:19 +0900

    update: comment out class matrix

--- src/y0_core/math/Matrix.cc
+++ src/y0_core/math/Matrix.cc
@@ -1,28 +1,28 @@
-#include "y0_core/math/Matrix.h"
-
-#include <algorithm>
-
-namespace y0_engine {
-  /**
-   * @brief constructor
-   */
-  Matrix::Matrix() {
-  }
-
-  /**
-   * @brief constructor to set default value
-   *
-   * @param a initilalize value
-   */
-  Matrix::Matrix(const GLfloat *a) {
-    std::copy(a, a + 16, matrix);
-  }
-
-  /**
-   * @brief let the matrix to identity matrix
-   */
-  void Matrix::load_identity() {
-    std::fill(matrix, matrix + 16, 0.0f);
-    matrix[0] = matrix[5] = matrix[10] = matrix[15] = 1.0f;
-  }
-}
+// #include "y0_core/math/Matrix.h"
+// 
+// #include <algorithm>
+// 
+// namespace y0_engine {
+//   /**
+//    * @brief constructor
+//    */
+//   Matrix::Matrix() {
+//   }
+// 
+//   /**
+//    * @brief constructor to set default value
+//    *
+//    * @param a initilalize value
+//    */
+//   Matrix::Matrix(const T *a) {
+//     std::copy(a, a + 16, matrix);
+//   }
+// 
+//   /**
+//    * @brief let the matrix to identity matrix
+//    */
+//   void Matrix::load_identity() {
+//     std::fill(matrix, matrix + 16, 0.0f);
+//     matrix[0] = matrix[5] = matrix[10] = matrix[15] = 1.0f;
+//   }
+// }
